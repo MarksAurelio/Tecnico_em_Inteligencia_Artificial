0793bd568e3c394ac81d7aa813a78a2bf0ec1ccf
Marks Aurelio <marksaurelio@yahoo.com>
2025-09-25 21:36:45 -0300

    desafios com operadores aritméticos

--- dev/desafio2.py
+++ dev/desafio2.py
@@ -1,1 +1,16 @@
-print("PuthonSchools")
+import os
+# print(""" PythonSchools """)
+
+# # variavel
+# nome_categoria = input("Digite o nome da categoria: ")
+# # print - mostrar na tela
+# print(nome_categoria)
+# print(type(nome_categoria))
+
+# os.system("pause")
+# os.system("clear")
+# os.system("mkdir nova_pasta")
+# os.system("notepad")
+# os.system("explorer")
+os.system("snippingtool")
+os.system('osk')
---
+++ dev/desafio3.py
@@ -0,0 +1,8 @@
+# Algoritmo para somar dois números
+x = int(input('Digite o valor de x: '))
+y = int(input('Digite o valor de y: '))
+# Operadores aritméticos
+# + - * > /
+soma = x + y
+print(soma)
+
